95cf09fbd53e871687db27ce6db570d83b955478
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2022-06-12 00:42:46 +0530

    Add files via upload

---
+++ 2nd Semester/JAVA/PackFiles/FibInt.java
@@ -0,0 +1,9 @@
+// 16. Write a program to create a multilevel package and also creates a reusable class to generate 
+//  Fibonacci series, where the function to generate fibonacii series is given in a different file 
+//  belonging to the same package.
+
+package PackFiles;
+
+public interface FibInt {
+    public int fib(int x);
+}
