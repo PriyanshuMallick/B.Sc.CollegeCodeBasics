d43e86feaa82a4504eec339a49c8af9ec84a19d3
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2022-06-12 00:58:48 +0530

    Update $3_DynamicArray.java

--- 2nd Semester/JAVA/$3_DynamicArray.java
+++ 2nd Semester/JAVA/$3_DynamicArray.java
@@ -1,13 +1,23 @@
 // 3. To learn use of single dimensional array by defining the array dynamically
 
+import java.util.Scanner;
+
 public class $3_DynamicArray {
     public static void main(String[] args) {
-        int array[];
-        array = new int[5];
-        for (int i = 0; i < 5; i++) {
-            array[i] = i;
-            System.out.println(array[i]);
-        }
-    }
+        int array[], n;
 
+        Scanner cin = new Scanner(System.in);
+
+        System.out.println("How many elements do you want?");
+        n = cin.nextInt();
+
+        array = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = i;
+            System.out.print(array[i] + " ");
+        }
+
+        cin.close();
+    }
 }
