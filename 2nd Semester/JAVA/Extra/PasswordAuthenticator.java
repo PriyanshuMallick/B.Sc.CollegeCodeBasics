5eb850e93ca50977a687982e03a020b3f312ce4a
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2022-07-31 00:52:15 +0530

    Add files via upload

---
+++ 2nd Semester/JAVA/Extra/PasswordAuthenticator.java
@@ -0,0 +1,82 @@
+// Suppose a valid computer password consist of 8 characters
+// The fisrt of which is a digit 1, 3 or 5. and the rest 7 digits are either english alphabets or digits
+
+import java.util.Scanner;
+// import java.lang.Integer;
+
+class InvalidPasswordException extends Exception {
+    InvalidPasswordException(String message) {
+        super(message);
+    }
+}
+
+public class PasswordAuthenticator {
+
+    static void validatePassword(String password) throws InvalidPasswordException {
+        int validPasswordLength = 8;
+
+        // Check password length
+
+        if (password.length() != validPasswordLength) {
+            throw new InvalidPasswordException("Password must be 8 characters long");
+        }
+
+        int F = 0;
+
+        // Check first character of password
+
+        char firstChar[] = { '1', '3', '5' };
+
+        for (char i : firstChar) {
+            System.out.println("First Character: " + password.charAt(0) + "  i: " + i);
+
+            if (password.charAt(0) == i) {
+                F = 1;
+                break;
+            }
+        }
+
+        if (F == 0) {
+            throw new InvalidPasswordException("First character must be the digit 1, 3, 5");
+        }
+        F = 0;
+
+        // Check rest of the password
+
+        for (int i = 1; i < validPasswordLength; i++) {
+            System.out.println("Check rest of the password");
+
+            byte uni = (byte) password.charAt(i);
+            System.out.println(i + " : " + password.charAt(i));
+            System.out.println("uni: " + uni);
+
+            if (uni >= 48 && uni <= 57 // 0 - 9
+                    || uni >= 65 && uni <= 90 // A - Z
+                    || uni >= 97 && uni <= 122) // a - z
+            {
+                continue;
+            }
+
+            if (F == 0) {
+                throw new InvalidPasswordException("Last 7 characters must be either english alphabets or digits");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+
+        System.out.println("Enter your Password :\n");
+        String password = cin.nextLine();
+
+        cin.close();
+
+        try {
+            System.out.println("In Try Statement :\n");
+            validatePassword(password);
+            System.out.println("Your password is correct");
+        } catch (InvalidPasswordException e) {
+            System.out.println("Invalid password : " + e);
+        }
+    }
+}
