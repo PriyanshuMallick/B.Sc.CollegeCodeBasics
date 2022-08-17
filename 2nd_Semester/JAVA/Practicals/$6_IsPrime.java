e8444a6154a62a334d09025fa2f292467fb355c5
Priyanshu Mallick <76875358+PriyanshuMallick@users.noreply.github.com>
2022-08-17 20:41:44 +0530

    Update

---
+++ 2nd_Semester/JAVA/Practicals/$6_IsPrime.java
@@ -0,0 +1,37 @@
+// 6. To check if a number is prime or not, by taking the number as input from the keyboard
+
+import java.lang.Math;
+import java.util.Scanner;
+
+public class $6_IsPrime {
+    public static Boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        } else if (num == 2 || num == 3) {
+            return true;
+        } else {
+            int x = (int) Math.ceil(Math.sqrt(num));
+            for (int i = 2; i <= x; i++) {
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int num = cin.nextInt();
+
+        if (isPrime(num)) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+
+        cin.close();
+    }
+}
