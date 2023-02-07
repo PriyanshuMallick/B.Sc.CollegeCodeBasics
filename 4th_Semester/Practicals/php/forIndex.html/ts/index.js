abc7012288d44b9bbc260558bf102f22cc907602
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-02-07 15:20:28 +0530

    The loco that you make me

--- 4th_Semester/Practicals/php/forIndex.html/ts/index.js
+++ 4th_Semester/Practicals/php/forIndex.html/ts/index.js
@@ -1,21 +1,21 @@
 // const indexTable = document.getElementById("indexTable") as HTMLTableElement;
-// const projectFiles = [
-//   {
-//     name: "Homepage",
-//     description: "The main page of the project",
-//     link: "homepage.html",
-//   },
-//   {
-//     name: "About Us",
-//     description: "Information about the project and its team",
-//     link: "about.html",
-//   },
-//   {
-//     name: "Contact Us",
-//     description: "Information on how to contact the team",
-//     link: "contact.html",
-//   },
-// ];
+const projectFiles = [
+    {
+        name: "Homepage",
+        description: "The main page of the project",
+        link: "homepage.html",
+    },
+    {
+        name: "About Us",
+        description: "Information about the project and its team",
+        link: "about.html",
+    },
+    {
+        name: "Contact Us",
+        description: "Information on how to contact the team",
+        link: "contact.html",
+    },
+];
 // for (const file of projectFiles) {
 //   const row = indexTable.insertRow();
 //   const nameCell = row.insertCell();
@@ -25,20 +25,24 @@
 //   descriptionCell.innerHTML = file.description;
 //   linkCell.innerHTML = `<a href="${file.link}">View</a>`;
 // }
-const projectFiles = [
-    { name: "Project 1", url: "project1.html" },
-    { name: "Project 2", url: "project2.html" },
-    { name: "Project 3", url: "project3.html" },
-    // Add more links as needed...
-];
-const grid = document.querySelector(".grid");
+// const projectFiles = [
+//   { name: "Project 1", url: "project1.html" },
+//   { name: "Project 2", url: "project2.html" },
+//   { name: "Project 3", url: "project3.html" },
+//   // Add more links as needed...
+// ];
+const indexTable = document.getElementById("indexTable");
 for (const file of projectFiles) {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item");
-    gridItem.innerHTML = `
-    <h2>${file.name}</h2>
-    <a href="${file.url}" target="_blank">View Project</a>
-  `;
-    grid.appendChild(gridItem);
+    const gridRow = document.createElement("div");
+    gridRow.innerHTML = `
+      <div class="grid-row">
+        <div class="grid-cell text-left">${file.name}</div>
+        <div class="grid-cell text-left">${file.description}</div>
+        <div class="grid-cell">
+          <a href="${file.link}" target="_blank">View Project</a>
+        </div>
+      </div>
+    `;
+    indexTable.appendChild(gridRow);
 }
 //# sourceMappingURL=index.js.map
