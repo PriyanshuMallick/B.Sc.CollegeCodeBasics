bc9021a126b757ab4c2021a23f728e5ba2153c7b
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-02-17 11:49:52 +0530

    🦊🦊

---
+++ 4th_Semester/Algorithms/1_insertionSort.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+// Funtion Prototypes
+void insetionSort(int arr[], int len);
+void printarr(int arr[], int len);
+
+// Global Var
+int comparision = 0;
+
+int main()
+{
+    int testSize = 100;
+    int arrsize = 0;
+
+    for (int i = 0; i < testSize; i++)
+    {
+        arrsize += 10;
+        int arr[arrsize];
+
+        for (int j = 0; j < arrsize; j++)
+        {
+            arr[j] = arrsize - j;
+        }
+
+        // printf("Unsorted:\n");
+        // printarr(arr, arrsize);
+
+        insetionSort(arr, arrsize);
+
+        // printf("Sorted:\n");
+        // printarr(arr, arrsize);
+
+        // printf("Comparision: %d\n", comparision);
+
+        printf("%d:\tList Size: %d\t\tNo. of comparisons: %d\n", i + 1, arrsize, comparision);
+    }
+    return 0;
+}
+
+void insetionSort(int arr[], int len)
+{
+    comparision = 0;
+
+    for (int i = 1; i < len; i++)
+    {
+        int j;
+        int tmp = arr[i];
+        // comparision++;
+        for (j = i - 1; j > -1 && arr[j] > tmp; j--, comparision++)
+        {
+            arr[j + 1] = arr[j];
+        }
+        arr[j + 1] = tmp;
+    }
+}
+
+void printarr(int arr[], int len)
+{
+    --len;
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d -> ", arr[i]);
+    }
+    printf("%d\n", arr[len]);
+}
