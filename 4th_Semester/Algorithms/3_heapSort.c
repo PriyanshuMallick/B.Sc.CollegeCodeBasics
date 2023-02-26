044996abcd6fff5556190939b554a88075a450b7
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-02-26 17:49:21 +0530

    + mergeSort in c ✨

--- 4th_Semester/Algorithms/3_heapSort.c
+++ 4th_Semester/Algorithms/3_heapSort.c
@@ -1,21 +1,98 @@
+// 3. Implement Heap Sort(The program should report the number of comparisons)
+
 #include <stdio.h>
 
 // Function Prototypes
-void heapSort(int arr[]);
-void heapify(int arr[], int n, int i);
+void printarr(int arr[], int len);
+void swap(int *a, int *b);
+void heapSort(int arr[], int len);
+void heapify(int arr[], int len, int i);
 
 // Global Variables
 int comparisons = 0;
 
 int main()
 {
+    int testSize = 1;
+    int arrSize = 0;
+
+    for (int i = 0; i < testSize; i++)
+    {
+        arrSize += 10;
+        int arr[arrSize];
+
+        for (int j = 0; j < arrSize; j++)
+        {
+            arr[j] = arrSize - j;
+        }
+
+        printf("Unsorted:\n");
+        printarr(arr, arrSize);
+
+        heapSort(arr, arrSize);
+
+        printf("Sorted:\n");
+        printarr(arr, arrSize);
+
+        printf("Comparisons: %d\n", comparisons);
+        // printf("%d:\tArray Size: %d\t\tNo. of comparisons: %d\n", i + 1, arrSize, comparisons);
+    }
+
     return 0;
 }
 
-void heapSort(int arr[])
+void heapSort(int arr[], int len)
 {
+    comparisons = 0;
+
+    for (int i = len / 2 - 1; i >= 0; i--)
+    {
+        heapify(arr, len, i);
+    }
+
+    for (int i = len - 1; i >= 0; i--)
+    {
+        swap(&arr[0], &arr[i]);
+        heapify(arr, i, 0);
+    }
 }
 
-void heapify(int arr[], int n, int i)
+void heapify(int arr[], int len, int i)
 {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < len && arr[left] > arr[largest])
+    {
+        largest = left;
+        comparisons++;
+    }
+
+    if (right < len && arr[right] > arr[largest])
+    {
+        largest = right;
+        comparisons++;
+    }
+
+    if (largest != i)
+    {
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, len, largest);
+    }
+}
+
+void swap(int *a, int *b)
+{
+    *a ^= *b ^= *a ^= *b;
+}
+
+void printarr(int arr[], int len)
+{
+    --len;
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d -> ", arr[i]);
+    }
+    printf("%d\n", arr[len]);
 }
