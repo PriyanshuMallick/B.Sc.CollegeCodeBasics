f2332f9f6fb88148156b65ba7ad3b81f6d9722b1
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-02-28 23:46:01 +0530

    👀👀

---
+++ 4th_Semester/Algorithms/3_heapSort.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Function Prototypes
+void heapSort(int arr[]);
+void heapify(int arr[], int n, int i);
+
+// Global Variables
+int comparisons = 0;
+
+int main()
+{
+    return 0;
+}
+
+void heapSort(int arr[])
+{
+}
+
+void heapify(int arr[], int n, int i)
+{
+}
