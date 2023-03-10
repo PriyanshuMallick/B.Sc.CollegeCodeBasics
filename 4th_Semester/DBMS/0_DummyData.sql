aa8d169950f8a1362cb0567b9c7ff8417a6d656c
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-03-10 23:43:16 +0530

    + Dummy Data for EMPLOYEE & DEPARTMENT Table

---
+++ 4th_Semester/DBMS/0_DummyData.sql
@@ -0,0 +1,43 @@
+-- Department Table
+INSERT INTO DEPARTMENT VALUES 
+(101, 'Development', 'Mumbai'),
+(102, 'Marketing', 'Bangalore'),
+(103, 'Design', 'New Delhi'),
+(104, 'IT Operations', 'Hyderabad'),
+(105, 'Finance', 'Chennai');
+
+-- Employee Table
+
+INSERT INTO EMPLOYEE VALUES
+('001', 'Manish Gupta', 'Development Manager', '001', '2020-01-01', 101, 4000.00, 80000.00),
+('002', 'Vikram Sharma', 'Marketing Manager', '002', '2020-09-01', 102, 4000.00, 80000.00),
+('003', 'Smita Patel', 'Design Manager', '003', '2020-05-01', 103, 5000.00, 60000.00),
+('004', 'Vishal Singh', 'IT Operations Manager', '004', '2020-02-01', 104, 5000.00, 80000.00),
+('005', 'Divya Mishra', 'Finance Manager', '005', '2020-08-01', 105, 4000.00, 85000.00);
+
+INSERT INTO EMPLOYEE VALUES
+('006', 'Sneha Sharma', 'Marketing Executive', '002', '2021-04-01', 102, 1500.00, 35000.00),
+('007', 'Rajat Gupta', 'QA Lead', '001', '2021-05-01', 101, 3500.00, 70000.00),
+('008', 'Sakshi Chauhan', 'Designer', '003', '2021-06-01', 103, 2000.00, 50000.00),
+('009', 'Ankur Verma', 'Developer', '001', '2021-07-01', 101, 2200.00, 52000.00),
+('010', 'Priyanka Singh', 'Marketing Lead', '002', '2021-08-01', 102, 2800.00, 58000.00),
+('012', 'Jatin Verma', 'QA Engineer', '001', '2021-10-01', 101, 1800.00, 45000.00),
+('013', 'Saurabh Singh', 'Designer', '003', '2021-11-01', 103, 2300.00, 53000.00),
+('014', 'Aarti Sharma', 'Developer', '001', '2021-12-01', 101, 1900.00, 48000.00),
+('015', 'Aditi Singh', 'Marketing Executive', '002', '2022-01-01', 102, 1700.00, 40000.00),
+('016', 'Nisha Sharma', 'Software Engineer', '001', '2022-01-01', 101, 2000.00, 75000.00),
+('017', 'Rajesh Kumar', 'Game Designer', '003', '2021-02-01', 103, 1500.00, 90000.00),
+('018', 'Rahul Sharma', 'Developer', '001', '2021-01-01', 101, 2000.00, 50000.00),
+('019', 'Priya Singh', 'Marketing Lead', '002', '2021-02-01', 102, 3000.00, 60000.00),
+('020', 'Rajesh Patel', 'Designer', '003', '2021-02-01', 103, 2500.00, 55000.00),
+('021', 'Amit Kumar', 'Developer', '001', '2021-03-01', 101, 1800.00, 45000.00),
+('022', 'Manish Gupta', 'Financial Analyst', '005', '2022-03-01', 105, 1800.00, 65000.00),
+('023', 'Nidhi Singh', 'Accountant', '005', '2022-04-01', 105, 1800.00, 50000.00),
+('024', 'Rahul Shah', 'Budget Analyst', '005', '2022-05-01', 105, 1800.00, 70000.00),
+('025', 'Priya Sharma', 'Security Analyst', '004', '2022-03-01', 104, 2000.00, 90000.00),
+('026', 'Saurabh Singh', 'System Administrator', '004', '2022-04-01', 104, 2000.00, 55000.00);
+
+
+
+
+
