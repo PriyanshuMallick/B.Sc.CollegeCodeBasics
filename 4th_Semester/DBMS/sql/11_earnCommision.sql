19c648aa42a65ae77694e942090d751da9770aa8
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-05-17 22:05:41 +0530

    Code Fix ✨✨

--- 4th_Semester/DBMS/sql/11_earnCommision.sql
+++ 4th_Semester/DBMS/sql/11_earnCommision.sql
@@ -1,6 +1,5 @@
--- 11. Query to display the Name, Salary and Commission for all the employees who earn 
--- commission.
+-- 11. Query to display the Name, Salary and Commission for all the employees who earn commission.
 
-SELECT Ename, Job_type
+SELECT Ename, Salary, Commission
 FROM EMPLOYEE
 WHERE Commission IS NOT NULL;
