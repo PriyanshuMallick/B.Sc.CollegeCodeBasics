0d6e42626756836b1c82f9b2765a915340bd94b0
priyanshumallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-06-26 11:43:13 +0530

    + Q22 JDBC

---
+++ 4th_Semester/DBMS/JDBC/$22_deptName&No.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class $22_deptName&No {
+    public static void main(String[] args) {
+        String DB_URL = "jdbc:mysql://localhost:3306/BSC_Practicals_JDBC";
+        String USER = "root";
+        String PASS = "";
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                Statement stmt = conn.createStatement()) {
+            String sql = """
+-- 22. Query to display Name, Department Name and Department No for all the employees.
+
+SELECT e.Ename, d.Dname, d.Dno
+FROM EMPLOYEE e
+JOIN DEPARTMENT d ON e.Dno = d.Dno;""";
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            System.out.println("Unique Jobs Are:");
+            while (rs.next()) {
+                String Job_type = rs.getString("Job_type");
+                System.out.println(Job_type);
+            }
+        } catch (Exception e) {
+            System.out.println("Exception occurred: " + e.getMessage());
+        }
+    }
+}
